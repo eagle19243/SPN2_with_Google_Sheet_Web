d4ab7e082bce940ebce17df891f97913ae85d176
Eagle19243 <eagle19243@gmail.com>
2018-08-02 17:07:37 +0300

    redirect uri changes

--- home/views.py
+++ home/views.py
@@ -54,7 +54,7 @@ def get_auth_uri():
     flow = client.OAuth2WebServerFlow(CLIENT_ID,
                                       CLIENT_SECRET,
                                       SCOPES,
-                                      'http://anton-dev.us.archive.org/')
+                                      'http://anton-dev.us.archive.org:8092/')
     auth_uri = flow.step1_get_authorize_url()
     return auth_uri
 
@@ -66,7 +66,7 @@ def process_doc(spreadsheet_id, auth_code):
     flow = client.OAuth2WebServerFlow(CLIENT_ID,
                                       CLIENT_SECRET,
                                       SCOPES,
-                                      'http://anton-dev.us.archive.org/')
+                                      'http://anton-dev.us.archive.org:8092/')
     creds = flow.step2_exchange(auth_code)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
