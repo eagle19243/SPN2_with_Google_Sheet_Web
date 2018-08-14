60df3868e8c360f366d9575141cf0bc150d91ea5
Eagle19243 <eagle19243@gmail.com>
2018-08-14 19:32:17 +0300

    add static asset, ias3 authorization to header

--- home/views.py
+++ home/views.py
@@ -9,35 +9,40 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file as oauth_file, client, tools
 
-SPN_URL = 'https://web-beta.archive.org/save/'
-LOGIN_URL = 'https://archive.org/account/login.php'
+SPN_URL = 'http://vbanos-dev.us.archive.org:8092/save/'
 AVAILABILITY_API_URL = 'https://archive.org/wayback/available'
-USERNAME = 'wbmloader@gmail.com'
-PASSWORD = 'wbm*loader'
 CLIENT_ID = '993382127942-iakt5sui2m26t4vg0ed1g7f0kt2kch4e.apps.googleusercontent.com'
 CLIENT_SECRET = '3JrJxLpmpkN3WezmwYKF4AhL'
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-HEADERS = {
-    'User-Agent': 'Wayback_Machine_SPN2_Google_Spreadsheet',
-    'Accept' : 'application/json'
-}
+REDIRECT_URI_DEV = 'http://localhost:8000/archive'
+REDIRECT_URI_PROD = 'http://anton-dev.us.archive.org:8092/archive'
+
+def index(request):
+    return render(request, 'index.html')
 
 @csrf_exempt
-def index(request):
+def archive(request):
     if request.method == 'POST':
         spreadsheet_id = get_spreadsheet_id_from_url(request.POST['google_sheet_url'])
+        s3_access_key = request.POST['access_key']
+        s3_secret_key = request.POST['secret_key']
 
         if not spreadsheet_id:
             return JsonResponse({'success': False, 'message': 'Invalid Spreadsheet URL'})
         else:
             auth_code = request.COOKIES.get('auth_code')
-            process_doc(spreadsheet_id, auth_code)
+            process_doc(spreadsheet_id, auth_code, {
+                'User-Agent': 'Wayback_Machine_SPN2_Google_Spreadsheet',
+                'Accept': 'application/json',
+                'authorization': 'LOW %s:%s' % (s3_access_key, s3_secret_key)
+            })
+
             return JsonResponse({'success': True})
 
     else:
         if 'code' in request.GET:
             code = request.GET['code']
-            response = render(request, 'index.html', {'message': 'Processing...'})
+            response = render(request, 'archive.html', {'message': 'Processing...'})
             response.set_cookie('auth_code', code)
             return response
         else:
@@ -54,19 +59,15 @@ def get_auth_uri():
     flow = client.OAuth2WebServerFlow(CLIENT_ID,
                                       CLIENT_SECRET,
                                       SCOPES,
-                                      'http://anton-dev.us.archive.org:8092/')
+                                      REDIRECT_URI_DEV)
     auth_uri = flow.step1_get_authorize_url()
     return auth_uri
 
-def process_doc(spreadsheet_id, auth_code):
-    session = requests.session()
-    session.get(LOGIN_URL)
-    session.post(url=LOGIN_URL, data={'username': USERNAME, 'password': PASSWORD, 'action': 'login'})
-
+def process_doc(spreadsheet_id, auth_code, headers):
     flow = client.OAuth2WebServerFlow(CLIENT_ID,
                                       CLIENT_SECRET,
                                       SCOPES,
-                                      'http://anton-dev.us.archive.org:8092/')
+                                      REDIRECT_URI_DEV)
     creds = flow.step2_exchange(auth_code)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
@@ -90,19 +91,18 @@ def process_doc(spreadsheet_id, auth_code):
                 if not is_valid_url(url):
                     continue
 
-                availability = check_availability(url, session)
-                job_id = request_capture(url, session)
+                availability = check_availability(url, headers)
+                job_id = request_capture(url, headers)
 
                 if not job_id:
                     continue
 
-                (status, captured_url) = request_capture_status(job_id, session)
+                (status, captured_url) = request_capture_status(job_id, headers)
 
                 update_values(service,
                               spreadsheet_id,
-                              sheet + '!B' + str(row_index) + ':E'+ str(row_index),
-                              [availability, status, captured_url, job_id])
-
+                              sheet + '!B' + str(row_index) + ':D'+ str(row_index),
+                              [availability, status, captured_url])
 
 def update_values(service, spreadsheet_id, range, values):
     body = {
@@ -115,8 +115,8 @@ def is_valid_url(url):
     match = re.match(r'(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?', url)
     return match is not None
 
-def request_capture(url, session):
-    response = session.get(url=SPN_URL + url, headers=HEADERS)
+def request_capture(url, headers):
+    response = requests.get(url=SPN_URL + url, headers=headers)
 
     try:
         data = response.json()
@@ -124,14 +124,17 @@ def request_capture(url, session):
     except:
         return None
 
-def request_capture_status(job_id, session):
-    time.sleep(20)
-    response = session.get(url=SPN_URL + '_status/' + job_id, headers=HEADERS)
+def request_capture_status(job_id, headers):
+    time.sleep(10)
+    response = requests.get(
+        url='%sstatus/%s?_t=%s' % (SPN_URL, job_id, str(time.time())),
+        headers=headers
+    )
 
     try:
         data = response.json()
         if data['status'] == 'pending':
-            return request_capture_status(job_id, session)
+            return request_capture_status(job_id, headers)
         else:
             if 'timestamp' in data and 'original_url' in data:
                 return (data['status'], 'http://web.archive.org/web/' + data['timestamp'] + '/' + data['original_url'])
@@ -140,8 +143,8 @@ def request_capture_status(job_id, session):
     except:
         return('Error: JSON parse', '')
 
-def check_availability(url, session):
-    response = session.get(url=AVAILABILITY_API_URL + '?url=' + url, headers=HEADERS)
+def check_availability(url, headers):
+    response = requests.get(url=AVAILABILITY_API_URL + '?url=' + url, headers=headers)
 
     if get_wayback_url_from_response(response.json()):
         return True
